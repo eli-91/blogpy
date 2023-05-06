c18b0cf7dcd567b5068324193a145d9a41cdcae9
اشرف - الهام <ashraf-e@tehran.iri>
2023-05-06 10:47:11 +0330

    'add_requirements'

--- blog/models.py
+++ blog/models.py
@@ -11,13 +11,12 @@ def validate_file_extension(value):
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file')
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False, validators=[validate_file_extension])
     description = models.CharField(max_length=512, null=False, blank=False)
 
-    def __str__(self):
-        return self.user.first_name + ' ' + self.user.last_name
 
 class Article(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
@@ -27,15 +26,11 @@ class Article(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
 
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False, validators=[validate_file_extension])
 
-    def __str__(self):
-        return self.title
 
 
 
